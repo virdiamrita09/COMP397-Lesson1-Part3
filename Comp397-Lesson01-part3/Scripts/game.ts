3440939e24bf0ed8aff28dc38f23a3bcdcb4e544
Amrita Virdi <virdi.amrita09@gmail.com>
2015-05-24 22:21:53 -0700

    final

--- Comp397-Lesson01-part3/Scripts/game.ts
+++ Comp397-Lesson01-part3/Scripts/game.ts
@@ -1,6 +1,18 @@
 ﻿ 
-
+var stage: createjs.Stage;
+var canvas= document.getElementById("canvas");
 
 function init() {
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+    main();
+}
+function gameLoop() {
+    stage.update();
+}
+function main() {
     console.log("Game is Running");
+    var helloText: createjs.Text = new createjs.Text("Hello World", "40 px Consolas", "#000000");
+    stage.addChild(helloText);
 }
